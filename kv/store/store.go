2ad79be9175ef2fc47dbb897c5d350858cc19e26
hatlonely <hatlonely@gmail.com>
2025-09-19 23:15:55 +0800

    feat(kv): 添加 KV 存储接口
    - 定义了 KV 存储相关的接口和选项
    - 实现了 WithExpiration 和 WithIfNotExist 两个选项
    - 接口包括 Set、Get、Del、BatchSet、BatchGet 和 BatchDel 等方法

---
+++ kv/store/store.go
@@ -0,0 +1,39 @@
+package kv
+
+import (
+	"context"
+	"time"
+)
+
+// Store KV 存储相关接口
+
+// setOptions 用于设置 KV 数据时的选项
+type setOptions struct {
+	Expiration time.Duration
+	IfNotExist bool
+}
+
+// setOption 用于设置 KV 数据时的选项
+type setOption func(*setOptions)
+
+func WithExpiration(expiration time.Duration) setOption {
+	return func(options *setOptions) {
+		options.Expiration = expiration
+	}
+}
+
+func WithIfNotExist() setOption {
+	return func(options *setOptions) {
+		options.IfNotExist = true
+	}
+}
+
+type Store[K, V any] interface {
+	Set(ctx context.Context, key K, value V, opts ...setOption) error
+	Get(ctx context.Context, key K) (V, error)
+	Del(ctx context.Context, key K) error
+	BatchSet(ctx context.Context, keys []K, vals []V, opts ...setOption) ([]error, error)
+	BatchGet(ctx context.Context, keys []K) ([]V, []error, error)
+	BatchDel(ctx context.Context, keys []K) ([]error, error)
+	Close() error
+}
