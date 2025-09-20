e1b61716c2a36f12d81900b7c67b65c200a1b1e3
hatlonely <hatlonely@gmail.com>
2025-09-20 23:06:40 +0800

    ``` test(kv/store): 更新 tar.gz 测试中使用的临时目录名称
    将测试中使用的目录名称从 "test" 相关名称统一修改为 "temp" 相关名称，
    以避免与实际测试环境中的其他文件或目录产生冲突，提高测试的独立性和稳定性。
    ```

--- kv/store/tar_gz_test.go
+++ kv/store/tar_gz_test.go
@@ -9,30 +9,30 @@ import (
 
 func TestTarGzip(t *testing.T) {
 	createTestDirectory := func() {
-		os.MkdirAll("test", os.ModePerm)
-		os.WriteFile("test/1.txt", []byte("hello world 1"), os.ModePerm)
-		os.WriteFile("test/2.txt", []byte("hello world 2"), os.ModePerm)
-		os.WriteFile("test/3.txt", []byte("hello world 3"), os.ModePerm)
+		os.MkdirAll("temp", os.ModePerm)
+		os.WriteFile("temp/1.txt", []byte("hello world 1"), os.ModePerm)
+		os.WriteFile("temp/2.txt", []byte("hello world 2"), os.ModePerm)
+		os.WriteFile("temp/3.txt", []byte("hello world 3"), os.ModePerm)
 	}
 
 	Convey("TestTarGzip", t, func() {
 		createTestDirectory()
-		defer os.RemoveAll("test")
-		defer os.RemoveAll("test.tar.gz")
-		defer os.RemoveAll("test1")
+		defer os.RemoveAll("temp")
+		defer os.RemoveAll("temp.tar.gz")
+		defer os.RemoveAll("temp1")
 
-		So(createTarGz("test", "test.tar.gz"), ShouldBeNil)
-		So(extractTarGz("test.tar.gz", "test1"), ShouldBeNil)
+		So(createTarGz("temp", "temp.tar.gz"), ShouldBeNil)
+		So(extractTarGz("temp.tar.gz", "temp1"), ShouldBeNil)
 
-		txt1, err := os.ReadFile("test1/1.txt")
+		txt1, err := os.ReadFile("temp1/1.txt")
 		So(err, ShouldBeNil)
 		So(string(txt1), ShouldEqual, "hello world 1")
 
-		txt2, err := os.ReadFile("test1/2.txt")
+		txt2, err := os.ReadFile("temp1/2.txt")
 		So(err, ShouldBeNil)
 		So(string(txt2), ShouldEqual, "hello world 2")
 
-		txt3, err := os.ReadFile("test1/3.txt")
+		txt3, err := os.ReadFile("temp1/3.txt")
 		So(err, ShouldBeNil)
 		So(string(txt3), ShouldEqual, "hello world 3")
 	})
