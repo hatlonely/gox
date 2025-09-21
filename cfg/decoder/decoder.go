e4d751af99fedd031e7e16da17504cdaca5379f1
hatlonely <hatlonely@gmail.com>
2025-09-22 01:00:22 +0800

    ``` feat(cfg/decoder): 添加 NewDecoderWithOptions 函数用于创建解码器实例
    新增的 NewDecoderWithOptions 函数支持通过 ref.TypeOptions 配置选项来创建解码器，
    并增加了对解码器类型的检查和错误处理，确保返回的解码器实例有效且符合接口要求。
    ```

--- cfg/decoder/decoder.go
+++ cfg/decoder/decoder.go
@@ -3,6 +3,7 @@ package decoder
 import (
 	"github.com/hatlonely/gox/cfg/storage"
 	"github.com/hatlonely/gox/ref"
+	"github.com/pkg/errors"
 )
 
 func init() {
@@ -28,4 +29,19 @@ type Decoder interface {
 	Decode(data []byte) (storage storage.Storage, err error)
 	// Encode 将存储对象编码为原始数据
 	Encode(storage storage.Storage) (data []byte, err error)
+}
+
+func NewDecoderWithOptions(options *ref.TypeOptions) (Decoder, error) {
+	decoder, err := ref.New(options.Namespace, options.Type, options.Options)
+	if err != nil {
+		return nil, errors.WithMessage(err, "refx.NewT failed")
+	}
+	if decoder == nil {
+		return nil, errors.New("decoder is nil")
+	}
+	if _, ok := decoder.(Decoder); !ok {
+		return nil, errors.New("decoder is not a Decoder")
+	}
+
+	return decoder.(Decoder), nil
 }
