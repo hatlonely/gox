cc954951e3a22f4ba77f06e18ca73dae9b7adc2a
hatlonely <hatlonely@gmail.com>
2025-09-27 00:10:19 +0800

    feat(rdb/sql): 添加SQL配置选项和初始化方法
    新增SQLOptions结构体用于配置SQL连接参数，包括主机地址和端口号。
    添加NewSQLWithOptions函数用于创建SQL实例。

--- rdb/sql.go
+++ rdb/sql.go
@@ -1,1 +1,13 @@
 package rdb
+
+type SQLOptions struct {
+	Host string `cfg:"host" def:"localhost"`
+	Port string `cfg:"port" def:"3306"`
+}
+
+type SQL struct {
+}
+
+func NewSQLWithOptions(options *SQLOptions) (*SQL, error) {
+	return &SQL{}, nil
+}
