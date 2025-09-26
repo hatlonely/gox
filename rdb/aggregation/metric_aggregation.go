2ced468e40fae2d3dc5b86393f0b7f659eb6f500
hatlonely <hatlonely@gmail.com>
2025-09-27 00:00:22 +0800

    ``` refactor(rdb/aggregation): 重构聚合结构以提升代码组织性
    将 MetricAggregation 和 BucketAggregation 结构体及其相关辅助函数从聚合接口文件中分离，
    分别移入独立的 metric_aggregation.go 和 bucket_aggregation.go 文件中，
    使代码结构更清晰、模块化程度更高。
    ```

--- rdb/aggregation/metric_aggregation.go
+++ rdb/aggregation/metric_aggregation.go
@@ -1,1 +1,11 @@
 package aggregation
+
+// MetricAggregation 指标聚合基础结构
+type MetricAggregation struct {
+	AggName string
+	Field   string
+}
+
+func (m *MetricAggregation) Name() string {
+	return m.AggName
+}
