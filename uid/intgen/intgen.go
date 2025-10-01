58a457c435171ffdc439050bf4ab4e172d7ae3b1
hatlonely <hatlonely@gmail.com>
2025-10-01 17:48:19 +0800

    feat(intgen): 注册时间戳序列和雪花算法生成器
    在intgen包中添加了对TimestampSeqGenerator和SnowflakeGenerator的注册逻辑，
    使其能够在运行时通过反射机制进行初始化和使用。

--- uid/intgen/intgen.go
+++ uid/intgen/intgen.go
@@ -1,5 +1,12 @@
 package intgen
 
+import "github.com/hatlonely/gox/ref"
+
+func init() {
+	ref.RegisterT[TimestampSeqGenerator](NewTimestampSeqGenerator)
+	ref.RegisterT[SnowflakeGenerator](NewSnowflakeGeneratorWithOptions)
+}
+
 // IntGenerator 生成64位整数UID的接口
 type IntGenerator interface {
 	// Generate 生成一个64位整数UID
