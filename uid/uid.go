6b2d9dce50cb9ab4df15ee81308831b3caefbbed
hatlonely <hatlonely@gmail.com>
2025-10-01 22:59:31 +0800

    ``` feat(uid): 添加整数和字符串生成器的工厂函数
    新增 NewIntGeneratorWithOptions 和 NewStrGeneratorWithOptions 函数，
    用于根据配置选项创建对应的整数和字符串生成器实例。
    ```

--- uid/uid.go
+++ uid/uid.go
@@ -1,1 +1,17 @@
 package uid
+
+import (
+	"github.com/hatlonely/gox/ref"
+	"github.com/hatlonely/gox/uid/intgen"
+	"github.com/hatlonely/gox/uid/strgen"
+)
+
+// NewIntGeneratorWithOptions 创建整数生成器
+func NewIntGeneratorWithOptions(options *ref.TypeOptions) (intgen.IntGenerator, error) {
+	return intgen.NewIntGeneratorWithOptions(options)
+}
+
+// NewStrGeneratorWithOptions 创建字符串生成器
+func NewStrGeneratorWithOptions(options *ref.TypeOptions) (strgen.StrGenerator, error) {
+	return strgen.NewStrGeneratorWithOptions(options)
+}
